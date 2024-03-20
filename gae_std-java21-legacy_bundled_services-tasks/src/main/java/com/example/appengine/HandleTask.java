7774e7b31fba299953eb32c6bb9faac8e878f1af
javalladares <javalladares@google.com>
2024-03-20 23:11:51 +0000

    removed unused imports

--- gae_std-java21-legacy_bundled_services-tasks/src/main/java/com/example/appengine/HandleTask.java
+++ gae_std-java21-legacy_bundled_services-tasks/src/main/java/com/example/appengine/HandleTask.java
@@ -22,10 +22,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.taskqueue.Queue;
-import com.google.appengine.api.taskqueue.QueueFactory;
-import com.google.appengine.api.taskqueue.TaskOptions;
-
 @WebServlet(name = "HandleTask", value = "/task/handle")
 public class HandleTask extends HttpServlet {
 
